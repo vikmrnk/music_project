9a95168fb608a7d90a6273b3af637742807941e8
vikmrnk <kmrnkviktoria@gmail.com>
2026-02-13 13:06:28 +0200

    feat: enhance article import command to create and activate categories with slug generation

--- articles/management/commands/import_articles.py
+++ articles/management/commands/import_articles.py
@@ -6,8 +6,27 @@ import json
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.utils.text import slugify
 from articles.models import Article, Category, Tag
 from datetime import datetime
+
+try:
+    from unidecode import unidecode
+except ImportError:
+    # Простий transliteration для української, якщо unidecode не встановлено
+    UKR_TO_LAT = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ie',
+        'ж': 'zh', 'з': 'z', 'и': 'y', 'і': 'i', 'ї': 'i', 'й': 'y', 'к': 'k', 'л': 'l',
+        'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+        'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ь': '', 'ю': 'iu', 'я': 'ia',
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'H', 'Ґ': 'G', 'Д': 'D', 'Е': 'E', 'Є': 'IE',
+        'Ж': 'Zh', 'З': 'Z', 'И': 'Y', 'І': 'I', 'Ї': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L',
+        'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
+        'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ь': '', 'Ю': 'Iu', 'Я': 'Ia'
+    }
+    
+    def unidecode(text):
+        return ''.join(UKR_TO_LAT.get(char, char) for char in text)
 
 
 class Command(BaseCommand):
@@ -356,7 +375,29 @@ class Command(BaseCommand):
                 # Отримати категорію
                 category = None
                 if article_data.get('category'):
-                    category = Category.objects.filter(name=article_data['category']).first()
+                    # Створити категорію, якщо її немає
+                    category, created = Category.objects.get_or_create(
+                        name=article_data['category'],
+                        defaults={
+                            'slug': slugify(unidecode(article_data['category'])),
+                            'is_active': True,
+                            'order': 0,  # Буде оновлено пізніше
+                        }
+                    )
+                    # Якщо категорія була створена або неактивна, зробити її активною
+                    if created or not category.is_active:
+                        category.is_active = True
+                        # Встановити порядок на основі назви
+                        category_order_map = {
+                            'Новини': 1,
+                            'Інтервʼю': 2,
+                            'Рецензії': 3,
+                            'Аналітика': 4,
+                            'Добірки': 5,
+                            'Авторські колонки': 6,
+                        }
+                        category.order = category_order_map.get(article_data['category'], 0)
+                        category.save()
                 
                 # Парсити дату публікації
                 published_at = None
